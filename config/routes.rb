1e91c6a8ea835958c8bdc8890484553cd35e30e0
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-19 17:59:37 +0100

    removed automatically generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'rentals/new'
-
-  get 'rentals/edit'
-
-  get 'cars/new'
-
-  get 'cars/edit'
-
-  get 'cars/show'
-
-  get 'cars/index'
-
   devise_for :users
   root to: 'cars#index'
   resources :cars do
