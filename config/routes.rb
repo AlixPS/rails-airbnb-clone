afe4651c8d60563c83cc431ce78f849a7d18a22b
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-19 17:27:23 +0100

    routes definies avec resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'cars#index'
+  resources :cars do
+    resources :rentals, only: [:new, :create, :edit, :update, :destroy] do
+      resources :reviews, only: [:new, :create]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
