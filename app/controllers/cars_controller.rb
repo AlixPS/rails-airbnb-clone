47bc3e16b1289a003304c648c91843b2288eb351
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-19 18:01:41 +0100

    added create methods

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,6 +2,9 @@ class CarsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
   end
 
--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,6 +2,9 @@ class RentalsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
   end
 end
