a461e98d9d1be995ede93c0740ca508ad82844f7
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-19 18:25:59 +0100

    a car needs a brand, a model, and a category

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,12 @@
 class Car < ApplicationRecord
+  CATEGORIES = %w[Citadine Compacte Monospace SUV Utilitaire].freeze
+
   belongs_to :user
   has_many :rentals
   has_many :reviews, through: :rentals
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :category, presence: true,
+                       inclusion: { in: CATEGORIES }
 end
