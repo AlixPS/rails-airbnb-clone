e99ad9e9d51dc82ceac4d983066e413d9cd01c55
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-19 18:07:51 +0100

    a user has many cars

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :rentals, through: :cars
+  has_many :cars
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
