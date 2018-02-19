fc88a47c8afa8f60409c2cf3bb719223a5136568
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-19 17:36:01 +0100

    fixed routes for good

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+Rails.application.routes.draw do
   devise_for :users
   root to: 'cars#index'
   resources :cars do
-    resources :rentals, only: [:new, :create, :edit, :update, :destroy] do
-      resources :reviews, only: [:new, :create]
-    end
+    resources :rentals, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :rentals, only: [] do
+    resources :reviews, only: [:new, :create]
   end
 end
