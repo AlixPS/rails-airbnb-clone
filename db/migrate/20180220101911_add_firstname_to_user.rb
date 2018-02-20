864537601fedcaa9b16da85c77952562c0bbea22
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-20 11:48:01 +0100

    added first name and last name, birthdate, driver licence

---
+++ db/migrate/20180220101911_add_firstname_to_user.rb
@@ -0,0 +1,8 @@
+class AddFirstnameToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birthdate, :date
+    add_column :users, :driver_licence, :string
+  end
+end
