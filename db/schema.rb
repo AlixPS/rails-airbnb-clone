626e08c8d04582fac637d6d3f1cee760f33330fb
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-20 18:01:28 +0100

    checkin and checkout columns cannot be null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220104837) do
+ActiveRecord::Schema.define(version: 20180220165944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,15 @@ ActiveRecord::Schema.define(version: 20180220104837) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
     t.bigint "car_id"
     t.bigint "user_id"
-    t.date "checkin"
-    t.date "checkout"
+    t.date "checkin", null: false
+    t.date "checkout", null: false
     t.boolean "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
