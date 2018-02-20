bb342acfc18b2b9f72efffd3a4b9368e6568a1b7
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-20 15:37:26 +0100

    add phto uploader set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220104837) do
+ActiveRecord::Schema.define(version: 20180220143328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180220104837) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
