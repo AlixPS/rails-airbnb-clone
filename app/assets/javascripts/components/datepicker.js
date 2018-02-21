04026971749c404619703a97624f86052199492a
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-21 11:48:07 +0100

    the checkout date starts today too

--- app/assets/javascripts/components/datepicker.js
+++ app/assets/javascripts/components/datepicker.js
@@ -1,10 +1,10 @@
 $(function() {
   today = new Date(Date.now());
   $('#checkin_datepicker').datepicker('setDate', today)
-                          .datepicker('setStartDate', today);                        
+                          .datepicker('setStartDate', today);
   $('#checkout_datepicker').datepicker({
-    useCurrent: false //Important! See issue #1075
-  });
+    useCurrent: false}) //Important! See issue #1075
+                           .datepicker('setStartDate', today)
 
   $("#checkin_datepicker").on("changeDate", function(e) {
     $('#checkout_datepicker').datepicker('setStartDate', e.date)
