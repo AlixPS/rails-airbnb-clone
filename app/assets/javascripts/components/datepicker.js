d8cb7b51bdced82c1d907213d810b51372306c41
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-21 11:18:25 +0100

    event 'changeDate' instead of 'dp.change'

--- app/assets/javascripts/components/datepicker.js
+++ app/assets/javascripts/components/datepicker.js
@@ -3,10 +3,10 @@ $(function() {
   $('#checkout_datepicker').datepicker({
     useCurrent: false //Important! See issue #1075
   });
-  $("#checkin_datepicker").on("dp.change", function(e) {
+  $("#checkin_datepicker").on("changeDate", function(e) {
     $('#checkout_datepicker').data("DateTimePicker").minDate(e.date);
   });
-  $("#checkout_datepicker").on("dp.change", function(e) {
+  $("#checkout_datepicker").on("changeDate", function(e) {
     $('#checkin_datepicker').data("DateTimePicker").maxDate(e.date);
   });
 });
