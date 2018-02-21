8c13797bcf554ad5005d86920aa9718475e2f9bc
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-21 13:25:25 +0100

    minimal skeleton to use bootstrap-datetimepicker

--- app/assets/javascripts/components/datepicker.js
+++ app/assets/javascripts/components/datepicker.js
@@ -1,17 +1,28 @@
 $(function() {
   const today = new Date(Date.now());
 
-  $('#checkin_datepicker').datepicker({language: 'fr'})
-                          .datepicker('setDate', today)
-                          .datepicker('setStartDate', today);
-  $('#checkout_datepicker').datepicker({useCurrent: false, //Important! See issue #1075
-                                        language: 'fr'})
-                           .datepicker('setStartDate', today)
+  // $('#checkin_datepicker').datepicker({language: 'fr'})
+  //                         .datepicker('setDate', today)
+  //                         .datepicker('setStartDate', today);
+  // $('#checkout_datepicker').datepicker({useCurrent: false, Important! See issue #1075
+  //                                       language: 'fr'})
+  //                          .datepicker('setStartDate', today)
+  //
+  // $("#checkin_datepicker").on("changeDate", function(e) {
+  //   $('#checkout_datepicker').datepicker('setStartDate', e.date)
+  // });
+  // $("#checkout_datepicker").on("changeDate", function(e) {
+  //   $('#checkin_datepicker').datepicker('setEndDate', e.date)
+  // });
 
-  $("#checkin_datepicker").on("changeDate", function(e) {
-    $('#checkout_datepicker').datepicker('setStartDate', e.date)
+  $('#checkin_datepicker ').datetimepicker();
+  $('#checkout_datepicker').datetimepicker({
+    useCurrent: false //Important! See issue #1075
   });
-  $("#checkout_datepicker").on("changeDate", function(e) {
-    $('#checkin_datepicker').datepicker('setEndDate', e.date)
+  $("#checkin_datepicker").on("dp.change", function(e) {
+    $('#checkout_datepicker').data("DateTimePicker").minDate(e.date);
+  });
+  $("#checkout_datepicker").on("dp.change", function(e) {
+    $('#checkin_datepicker').data("DateTimePicker").maxDate(e.date);
   });
 });
