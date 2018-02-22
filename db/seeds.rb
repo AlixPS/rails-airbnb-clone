365bdbdd9cce835003ea8bf93f8b368ef051bced
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-22 15:12:41 +0100

    add new validation for car model and adjust car form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,52 +5,92 @@ CARS_LIST = [
   {
     brand: 'Tesla',
     model: 'S',
-    category: 'Citadine'
+    category: 'Citadine',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Renault',
     model: 'Clio 3',
-    category: 'Citadine'
+    category: 'Citadine',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Citroën',
     model: 'C5',
-    category: 'Monospace'
+    category: 'Monospace',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Jeep',
     model: 'Wrangler',
-    category: 'SUV'
+    category: 'SUV',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Mercedes',
     model: 'Sprinter',
-    category: 'Utilitaire'
+    category: 'Utilitaire',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Citroën',
     model: 'Kangoo',
-    category: 'Utilitaire'
+    category: 'Utilitaire',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Peugeot',
     model: '308',
-    category: 'Compacte'
+    category: 'Compacte',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Mercedes',
     model: 'AMG',
-    category: 'Compacte'
+    category: 'Compacte',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'BMW',
     model: 'i3',
-    category: 'Compacte'
+    category: 'Compacte',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   },
   {
     brand: 'Fiat',
     model: 'Multipla',
-    category: 'Utilitaire'
+    category: 'Utilitaire',
+    places: ,
+    portes: ,
+    moteur: ,
+    boite:
   }
 ].freeze
 
@@ -61,11 +101,16 @@ CARS_LIST.length.times do |i|
       brand: CARS_LIST[i][:brand],
       model: CARS_LIST[i][:model],
       category: CARS_LIST[i][:category],
+      places: CARS_LIST[i][:places],
+      portes: CARS_LIST[i][:portes],
+      moteur: CARS_LIST[i][:moteur],
+      boite: CARS_LIST[i][:boite],
       user: User.create(email: Faker::Internet.email,
                         password: 'azerty',
                         first_name: char.split[0],
                         last_name:  char.split[1],
-                        birthdate: Faker::Date.birthday(18, 65))
+                        birthdate: Faker::Date.birthday(18, 65),
+                        avatar_photo: File.open("/assets/images/profil.jpg"))
     }
   ].freeze
   Car.create(cars)
