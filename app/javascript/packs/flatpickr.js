9bc4d3aaf66e7b8eee3064ffe332c268e127f69e
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-22 14:46:46 +0100

    the checkin date can't be before today

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -2,7 +2,13 @@ import flatpickr from 'flatpickr'
 import { French } from "flatpickr/dist/l10n/fr.js"
 import 'flatpickr/dist/flatpickr.css'
 
+const today = new Date(Date.now());
+
 flatpickr('.datepicker', {
   enableTime: true,
   locale: French
-})
+});
+
+flatpickr('#checkin_datepicker', {
+  minDate: today
+});
