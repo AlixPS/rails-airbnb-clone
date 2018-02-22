00ab3bb49fec3d3421c7e6686e61da714eda559e
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-22 16:35:10 +0100

    add the map config and works on local

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  geocoded_by :full_address
+  after_validation :geocode, if: :will_save_change_to_adress?
   has_many :rentals, through: :cars
   has_many :cars
   mount_uploader :avatar_photo, PhotoUploader
@@ -8,4 +10,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_address
+    "#{adress} #{city}"
+  end
 end
